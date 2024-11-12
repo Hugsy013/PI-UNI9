67f5caaa404343f56170d8957bc9169ed17f058a
Hugsy013 <rodrigo.fernandesdos@gmail.com>
2024-11-12 17:01:38 -0300

    alteração em login.php
    correção de bugs

--- SIM-Sistema_Integrado_ao_Mecanico/login.php
+++ SIM-Sistema_Integrado_ao_Mecanico/login.php
@@ -4,10 +4,12 @@ require_once("conexaobd.php");
 
 <?php
 session_start();
-if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
-    header('Location: dashboard.php');
-    exit;
-}
+//if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
+  //  header('Location: index.php');
+    //exit;
+//}
+
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
@@ -15,12 +17,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 $login = "SELECT * FROM tb_admin where login = '{$username}' and senha = '{$password}'";
 $rs_login = mysqli_query($conn_bd_sim, $login) or die($mysqli_error($conn_bd_sim));
 $linha_login = mysqli_num_rows($rs_login);
+$row_rs_login = mysqli_fetch_assoc($rs_login);
 
     // Verificação simples (para exemplo; substituir por verificação de banco de dados)
     if ($linha_login == 1) {
         $_SESSION['loggedin'] = true;
         $_SESSION['username'] = $username;
-        header('Location: dashboard.php');
+        $_SESSION['idAdmin'] = $row_rs_login['idAdmin']; 
+        header('Location: index.php');
         exit;
     } else {
         $error = 'Nome de usuário ou senha incorretos!';
@@ -43,12 +47,12 @@ $linha_login = mysqli_num_rows($rs_login);
             <h2>Seja bem-vindo à plataforma SIM</h2>
             <p>Faça login abaixo para acessar</p>
             <?php if (isset($error)) echo "<p style='color: red;'>$error</p>"; ?>
-            <form action="index.php" method="POST">
+            <form action="" method="POST">
                 <div class="input-group">
-                    <input type="text" name="username" placeholder="Digite o seu nome de usuário" required>
+                    <input type="text" name="username" id="username" placeholder="Digite o seu nome de usuário" required>
                 </div>
                 <div class="input-group">
-                    <input type="password" name="password" placeholder="Digite a sua senha" required>
+                    <input type="password" name="password" id="password" placeholder="Digite a sua senha" required>
                 </div>
                 <a href="forgot.html" class="forgot-password">Esqueci minha senha</a>
                 <button type="submit" class="login-btn">Login</button>
